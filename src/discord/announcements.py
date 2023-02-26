3c0a3790084d72b3bc7d626cd8b58d449d0607f7
Rdoolittle <Rdoolittle90@gmail.com>
2023-02-26 00:31:39 -0600

    repair remove_map modal

--- src/discord/announcements.py
+++ src/discord/announcements.py
@@ -1,4 +1,4 @@
-from disnake import ApplicationCommandInteraction, Color, Embed
+from nextcord import ApplicationCommandInteraction, Colour, Embed
 import datetime
 
 from src.discord.guild_manager import get_server_settings
@@ -14,14 +14,14 @@ async def announce_status(interaction:ApplicationCommandInteraction, status_code
         server_str_format = "**SERVER** is"
 
     if status_code == 0:
-        embed = Embed(title=f"**{map} {server_str_format} **DOWN", description=message, timestamp=datetime.datetime.now(), color=Color.red())
-        embed.set_author(name=interaction.author, icon_url=interaction.author.avatar.url)
+        embed = Embed(title=f"**{map} {server_str_format} **DOWN", description=message, timestamp=datetime.datetime.now(), color=Colour.red())
+        embed.set_author(name=interaction.user, icon_url=interaction.user.avatar.url)
     elif status_code == 1:
-        embed = Embed(title=f"**{map}** {server_str_format} now **ONLINE**", description=message, timestamp=datetime.datetime.now(), color=Color.green())
-        embed.set_author(name=interaction.author, icon_url=interaction.author.avatar.url)
+        embed = Embed(title=f"**{map}** {server_str_format} now **ONLINE**", description=message, timestamp=datetime.datetime.now(), color=Colour.green())
+        embed.set_author(name=interaction.user, icon_url=interaction.user.avatar.url)
     else:
-        embed = Embed(title=f"**{map}** {server_str_format} now **RESTARTING**", description=message, timestamp=datetime.datetime.now(), color=Color.yellow())
-        embed.set_author(name=interaction.author, icon_url=interaction.author.avatar.url)
+        embed = Embed(title=f"**{map}** {server_str_format} now **RESTARTING**", description=message, timestamp=datetime.datetime.now(), color=Colour.yellow())
+        embed.set_author(name=interaction.user, icon_url=interaction.user.avatar.url)
 
     settings = get_server_settings(interaction.guild.id)
     if settings["announcement_channel"] != None:
--- src/discord/modals/remove_map_modal.py
+++ src/discord/modals/remove_map_modal.py
@@ -1,5 +1,5 @@
-from disnake import Color, Embed, ModalInteraction
-from disnake.ui import Modal, TextInput
+from nextcord import Color, Embed, Interaction
+from nextcord.ui import Modal, TextInput
 
 from src.file_manager import get_map_key, remove_embed, remove_map_dir
 
@@ -7,36 +7,35 @@ from src.file_manager import get_map_key, remove_embed, remove_map_dir
 
 class RemoveMapModal(Modal):
     def __init__(self) -> None:
-        components: list = [
-            TextInput(
-                label="Map Name",
-                placeholder="Namalsk",
-                custom_id=f"map",
-                min_length=1,
-                max_length=25,
-                required=True
-            ),
-            TextInput(
-                label="Enter passkey",
-                placeholder="use /get_key",
-                custom_id=f"passkey",
-                min_length=16,
-                max_length=16,
-                required=True
-            )
-        ]
-        super().__init__(title="Select an Option Catagory", components=components)
-    
+        super().__init__(title="Select an Option Catagory")
+        self.map_name = TextInput(
+            label="Map Name",
+            placeholder="Namalsk",
+            custom_id=f"map",
+            min_length=1,
+            max_length=25,
+            required=True
+        )
+        self.add_item(self.map_name)
 
-    async def callback(self, interaction:ModalInteraction) -> None:
-        await interaction.response.defer(ephemeral=False)
-        map_str: str = interaction.data["components"][0]["components"][0]["value"]
-        passkey: str = interaction.data["components"][1]["components"][0]["value"]
-        map_key: str = get_map_key(interaction.guild.id, map_str)["passkey"]
+        self.map_key = TextInput(
+            label="Enter passkey",
+            placeholder="use /get_key",
+            custom_id=f"passkey",
+            min_length=16,
+            max_length=16,
+            required=True
+        )
+        self.add_item(self.map_key)
         
-        if passkey == map_key:
-            remove_map_dir(interaction.guild.id, map_str)
-            await interaction.followup.send(embed=remove_embed(map_str))
+
+    async def callback(self, interaction: Interaction) -> None:
+        await interaction.response.defer(ephemeral=False)
+        map_key: str = get_map_key(self.map_name.value)["passkey"]
+        
+        if self.map_key.value == map_key:
+            remove_map_dir(self.map_name.value)
+            await interaction.followup.send(embed=remove_embed(self.map_name.value))
         else:
             failed_deletion = Embed(
                 title="Map Deletion Failed!", 
