23eee382597a29661184d73a25cf667c252926ce
Rdoolittle <Rdoolittle90@gmail.com>
2023-02-25 20:19:35 -0600

    adding role requirements to commands

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from os import getenv
-import aiohttp
 
 from disnake import ApplicationCommandInteraction, Intents
-from disnake.ext.commands import when_mentioned, has_role
+from disnake.ext.commands import when_mentioned, check
 from dotenv import load_dotenv
-from src.discord.registration import EnterSteamID, verify_user
-from src.discord.guild_manager import set_announce_channel
-from src.discord.announcements import announce_status
 
+from src.discord.announcements import announce_status
 from src.discord.bot import DiscordBot
-from src.discord.guild_manager import get_map_selections
+from src.discord.guild_manager import get_map_selections, set_announce_channel
 from src.discord.load_traderconfig import load_traderconfig_view
 from src.discord.modals.remove_map_modal import RemoveMapModal
+from src.discord.registration import EnterSteamID
 from src.file_manager import create_new_map_dir, get_map_key, key_embed
-import aiohttp
-import aiomysql
-from datetime import datetime
+from disnake.utils import get
+
 
 
 def main():
@@ -33,6 +30,12 @@ def main():
     # this is the discord bot object
     bot = DiscordBot(command_prefix=prefix, intents=intents)
     bot.openai_api_key = "api_key_here"
+
+    def has_role(role_name):
+        async def predicate(ctx):
+            role = get(ctx.guild.roles, name=role_name)
+            return role in ctx.author.roles
+        return check(predicate)
 
     # below are all of the commands for the bot
     # default_member_permissions=8 is the same as saying only available to admins
