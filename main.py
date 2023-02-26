ee8c0353cba31c5648fd340037410973a5af9f98
Rdoolittle <Rdoolittle90@gmail.com>
2023-02-25 20:21:03 -0600

    ..

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 from disnake import ApplicationCommandInteraction, Intents
-from disnake.ext.commands import when_mentioned, check
+from disnake.ext.commands import when_mentioned, has_role
 from dotenv import load_dotenv
 
 from src.discord.announcements import announce_status
@@ -11,7 +11,7 @@ from src.discord.load_traderconfig import load_traderconfig_view
 from src.discord.modals.remove_map_modal import RemoveMapModal
 from src.discord.registration import EnterSteamID
 from src.file_manager import create_new_map_dir, get_map_key, key_embed
-from disnake.utils import get
+
 
 
 
@@ -30,12 +30,6 @@ def main():
     # this is the discord bot object
     bot = DiscordBot(command_prefix=prefix, intents=intents)
     bot.openai_api_key = "api_key_here"
-
-    def has_role(role_name):
-        async def predicate(ctx):
-            role = get(ctx.guild.roles, name=role_name)
-            return role in ctx.author.roles
-        return check(predicate)
 
     # below are all of the commands for the bot
     # default_member_permissions=8 is the same as saying only available to admins
