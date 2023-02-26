47c0d53864a8e0b158cb6e7c83c6d9ebb29c8752
Rdoolittle <Rdoolittle90@gmail.com>
2023-02-25 19:32:54 -0600

    ..

--- src/discord/registration.py
+++ src/discord/registration.py
@@ -44,13 +44,18 @@ class EnterSteamID(Modal):
         new_commit = await verify_user(interaction.bot, steam_id, interaction.author.id)
 
         # If the verification is successful, respond with a success message.
-        if new_commit:
+        if new_commit == 1:
             await interaction.followup.send("Success! thank you for registering.")
         # If the verification fails, respond with a failure message.
-        await interaction.followup.send(f"Fail! verify you entered the Steam 64 ID correctly: `{steam_id}`")
+        elif new_commit == -1:
+            await interaction.followup.send(f"Fail! it looks like that account is already registered: `{steam_id}`")
+        elif new_commit == -2:
+            await interaction.followup.send(f"Fail! it looks like you are already registered: `{steam_id}`")
+        else:
+            await interaction.followup.send(f"Fail! verify you entered the Steam 64 ID correctly: `{steam_id}`")
 
 
-async def verify_user(bot: DiscordBot, steam_id: str, discord_id: str) -> Optional[bool]:
+async def verify_user(bot: DiscordBot, steam_id: str, discord_id: str) -> Optional[int]:
     """
     Verifies the user's Steam ID by checking if their Discord ID is already associated with a Steam ID in the database and
     saves the Steam ID and user location information to the database if verification is successful.
@@ -66,16 +71,22 @@ async def verify_user(bot: DiscordBot, steam_id: str, discord_id: str) -> Option
     # Check if discord id is already associated with a steam id in the database
     existing_steam_id = await bot.sql_execute("SELECT steam_id FROM registration WHERE discord_id=%s", (discord_id,))
 
+    # Check if steam id is already associated with a discord id in the database
+    existing_discord_id = await bot.sql_execute("SELECT discord_id FROM registration WHERE steam_id=%s", (steam_id,))
+
+    if existing_discord_id is not None:
+        # Steam id already associated with a discord id
+        return -1
     if existing_steam_id is not None:
         # Discord id already associated with a steam id
-        return False
+        return -2
 
     # Get user location information from Steam API
     async with aiohttp.ClientSession() as session:
         async with session.get(f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={getenv("STEAM_API_KEY")}&steamids={steam_id}') as response:
             if response.status != 200:
                 # Handle error response
-                return False
+                return -3
 
             data = await response.json()
             user_info = data['response']['players'][0]
@@ -101,4 +112,4 @@ async def verify_user(bot: DiscordBot, steam_id: str, discord_id: str) -> Option
             # Insert new user data into database
             current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             await bot.sql_execute("INSERT INTO registration (discord_id, steam_id, loccountrycode, profile_url, registration_date) VALUES (%s, %s, %s, %s, %s)", discord_id, steam_id, loccountrycode, user_info.get('profileurl', None), current_time)
-            return True
+            return 1
