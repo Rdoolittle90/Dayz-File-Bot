99ba27a55fb55151050b340c35d4e162bda3c9fe
Rdoolittle <Rdoolittle90@gmail.com>
2023-03-01 17:39:18 -0600

    updating commands for dayz_user

--- src/discord/cogs/dayz_user_commands.py
+++ src/discord/cogs/dayz_user_commands.py
@@ -1,4 +1,4 @@
-from nextcord.ext import commands
+from nextcord.ext import commands, Embed
 import nextcord
 from src.discord.modals.registration import EnterSteamID
 from src.discord.bot import DiscordBot
@@ -11,34 +11,42 @@ class DayzUserCog(commands.Cog):
         print("Dayz User Cog Connected")
 
     # =====================================================================================================
-    @nextcord.slash_command(dm_permission=False, name="register", description="placeholder description 1")
+    @nextcord.slash_command(dm_permission=False, name="register", description="register your steam 64 ID")
     async def register(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        await interaction.response.send_modal(modal=EnterSteamID(self.bot))
+        embed = Embed(title="Unavailable", color=0xff0000)
+        embed.description = f"Command Coming Soon"
+        await interaction.channel.send(embed=embed)
 
 
     # =====================================================================================================
     @commands.has_role("Steam Linked")
-    @nextcord.slash_command(dm_permission=False, name="atm", description="placeholder description 1")
+    @nextcord.slash_command(dm_permission=False, name="atm", description="View your ATMs")
     async def atm(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        pass
+        embed = Embed(title="Unavailable", color=0xff0000)
+        embed.description = f"Command Coming Soon"
+        await interaction.channel.send(embed=embed)
 
 
     # =====================================================================================================
     @commands.has_role("Steam Linked")
-    @nextcord.slash_command(dm_permission=False, name="inventory", description="placeholder description 1")
+    @nextcord.slash_command(dm_permission=False, name="inventory", description="View your discord item inventory")
     async def inventory(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        pass
+        embed = Embed(title="Unavailable", color=0xff0000)
+        embed.description = f"Command Coming Soon"
+        await interaction.channel.send(embed=embed)
 
 
     # =====================================================================================================
     @commands.has_role("Steam Linked")
-    @nextcord.slash_command(dm_permission=False, name="trade", description="placeholder description 1")
+    @nextcord.slash_command(dm_permission=False, name="trade", description="Trade Discord Stashed items")
     async def trade(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        pass
+        embed = Embed(title="Unavailable", color=0xff0000)
+        embed.description = f"Command Coming Soon"
+        await interaction.channel.send(embed=embed)
 
 
 def setup(bot: commands.Bot):
