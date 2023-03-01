73040eeac40b4eb503061a4644bf3a700a88d273
Rdoolittle <Rdoolittle90@gmail.com>
2023-03-01 17:40:44 -0600

    ..

--- src/discord/cogs/dayz_user_commands.py
+++ src/discord/cogs/dayz_user_commands.py
@@ -1,4 +1,4 @@
-from nextcord.ext import commands, Embed
+from nextcord.ext import commands
 import nextcord
 from src.discord.modals.registration import EnterSteamID
 from src.discord.bot import DiscordBot
@@ -14,7 +14,7 @@ class DayzUserCog(commands.Cog):
     @nextcord.slash_command(dm_permission=False, name="register", description="register your steam 64 ID")
     async def register(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        embed = Embed(title="Unavailable", color=0xff0000)
+        embed = nextcord.Embed(title="Unavailable", color=0xff0000)
         embed.description = f"Command Coming Soon"
         await interaction.channel.send(embed=embed)
 
@@ -24,7 +24,7 @@ class DayzUserCog(commands.Cog):
     @nextcord.slash_command(dm_permission=False, name="atm", description="View your ATMs")
     async def atm(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        embed = Embed(title="Unavailable", color=0xff0000)
+        embed = nextcord.Embed(title="Unavailable", color=0xff0000)
         embed.description = f"Command Coming Soon"
         await interaction.channel.send(embed=embed)
 
@@ -34,7 +34,7 @@ class DayzUserCog(commands.Cog):
     @nextcord.slash_command(dm_permission=False, name="inventory", description="View your discord item inventory")
     async def inventory(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        embed = Embed(title="Unavailable", color=0xff0000)
+        embed = nextcord.Embed(title="Unavailable", color=0xff0000)
         embed.description = f"Command Coming Soon"
         await interaction.channel.send(embed=embed)
 
@@ -44,7 +44,7 @@ class DayzUserCog(commands.Cog):
     @nextcord.slash_command(dm_permission=False, name="trade", description="Trade Discord Stashed items")
     async def trade(self, interaction: nextcord.Interaction) -> None:
         """placeholder"""
-        embed = Embed(title="Unavailable", color=0xff0000)
+        embed = nextcord.Embed(title="Unavailable", color=0xff0000)
         embed.description = f"Command Coming Soon"
         await interaction.channel.send(embed=embed)
 
