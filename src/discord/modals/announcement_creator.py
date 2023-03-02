98537f5fd85f17f16784d55c9dbee1bf0d8d458b
Rdoolittle <Rdoolittle90@gmail.com>
2023-03-01 23:01:14 -0600

    ..

--- src/discord/modals/announcement_creator.py
+++ src/discord/modals/announcement_creator.py
@@ -1,4 +1,5 @@
 import datetime
+from discord import TextInputStyle
 from nextcord.ui import Modal, TextInput
 from nextcord import Interaction, Embed
 from src.discord.guild_manager import get_server_settings
@@ -17,10 +18,25 @@ class AnnouncementCreator(Modal):
         self.bot: DiscordBot = bot
         self.is_preview = preview
 
+        color_id = bot.generate_random_string(8)
+        title_id = bot.generate_random_string(8)
+        descr_id = bot.generate_random_string(8)
+
+        
+        self.color = TextInput(
+            label="Color",
+            placeholder="enter color",
+            custom_id=f"title_{color_id}",
+            min_length=1,
+            max_length=35,
+            required=False
+        )
+        self.add_item(self.color)
+
         self.title = TextInput(
             label="Title",
             placeholder="enter title",
-            custom_id=f"title_{bot.generate_random_string(8)}",
+            custom_id=f"title_{title_id}",
             min_length=1,
             max_length=35,
             required=True
@@ -28,11 +44,12 @@ class AnnouncementCreator(Modal):
         self.add_item(self.title)
 
         self.description = TextInput(
+            style=TextInputStyle.paragraph,
             label="Description",
             placeholder="enter description",
-            custom_id=f"description_{bot.generate_random_string(8)}",
+            custom_id=f"description_{descr_id}",
             min_length=1,
-            max_length=35,
+            max_length=1000,
             required=True
         )
         self.add_item(self.description)
@@ -44,10 +61,16 @@ class AnnouncementCreator(Modal):
         """
         await interaction.response.defer(ephemeral=False)
 
+
+        if not self.color.value:
+            self.color.value = 0xffffff
+
+
         # Create a new Embed object with the input values
         embed = Embed(
             title=self.title.value,
             description=self.description.value,
+            color=self.color.value,
             timestamp=datetime.datetime.utcnow()
         )
 
