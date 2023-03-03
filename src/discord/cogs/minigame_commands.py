a964e33c3865c1c0609ff3b1d11e92b960619c55
Rdoolittle <Rdoolittle90@gmail.com>
2023-03-03 03:17:05 -0600

    ..

--- src/discord/cogs/minigame_commands.py
+++ src/discord/cogs/minigame_commands.py
@@ -18,16 +18,15 @@ class MiniGames(commands.Cog):
         print("Minigame Cog Connected")
     
     @nextcord.slash_command(dm_permission=False, name="slots", description="Slots test")
-    async def slots(self, ctx, bet: int):
+    async def slots(self, interaction, bet: int):
         if bet <= 0:
-            await ctx.send("Please enter a valid bet amount.")
+            await interaction.channel.send("Please enter a valid bet amount.")
             return
         
-        user = ctx.user
         user_balance = 500 # replace with actual user balance
         
         if bet > user_balance:
-            await ctx.send(f"You do not have enough currency to place this bet. Your current balance is {user_balance}.")
+            await interaction.channel.send(f"You do not have enough currency to place this bet. Your current balance is {user_balance}.")
             return
         
         user_balance -= bet
@@ -47,12 +46,11 @@ class MiniGames(commands.Cog):
         user_balance += bet * payout
         
         embed = nextcord.Embed(title="DayZ Slot Machine", color=0xff0000)
-        embed.set_author(name=user.name, icon_url=user.avatar_url)
         embed.add_field(name="Reels", value=f"{reels[0]} | {reels[1]} | {reels[2]}", inline=False)
         embed.add_field(name="Payout", value=f"{bet * payout} currency")
         embed.add_field(name="Balance", value=f"{user_balance} currency", inline=False)
         
-        await ctx.send(embed=embed)
+        await interaction.channel.send(embed=embed)
 
 def setup(bot: commands.Bot):
     bot.add_cog(MiniGames(bot))
