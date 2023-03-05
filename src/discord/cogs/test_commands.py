5c5cb1f927caf7d59f39755673e4c9df375ad238
Rdoolittle <Rdoolittle90@gmail.com>
2023-03-05 01:04:28 -0600

    ..

--- src/discord/cogs/test_commands.py
+++ src/discord/cogs/test_commands.py
@@ -1,10 +1,50 @@
 import asyncio
+import hashlib
+from os import getenv
 from nextcord.ext import commands
 import nextcord
+import requests
 
 from src.discord.bot import DiscordBot
 from tester import make_authenticated_request
 
+
+def generate_server_id(game_identifier: int, ipv4: str, game_port: int) -> str:
+    # Build the string using the given parameters
+    server_string = f"{game_identifier}{ipv4}{game_port}"
+    
+    # Hash the string using SHA-1
+    hash_object = hashlib.sha1(server_string.encode())
+    
+    # Get the hex digest of the hash
+    hex_digest = hash_object.hexdigest()
+    
+    # Return the hex digest as the server ID
+    return hex_digest
+
+
+def make_authenticated_request(method, url, token=None, json=None):
+    headers = {}
+    if token:
+        headers["Authorization"] = f"Bearer {token}"
+
+    response = requests.request(method, url, headers=headers, json=json)
+    if response.status_code == 401 and response.json().get("error") == "expired-token":
+        # Token has expired, reauthenticate
+        token = authenticate()
+        headers["Authorization"] = f"Bearer {token}"
+        response = requests.request(method, url, headers=headers, json=json)
+        response.raise_for_status()
+
+    return response
+
+
+def authenticate():
+    auth_url = "https://data.cftools.cloud/v1/auth/register"
+    payload = {"application_id": getenv("CFTools_App_ID"), "secret": getenv("CFTools_secret")}
+    response = requests.post(auth_url, json=payload)
+    response.raise_for_status()
+    return response.json()["token"]
 
 class TestingCog(commands.Cog):
     def __init__(self, bot):
@@ -28,7 +68,8 @@ class TestingCog(commands.Cog):
     async def get_server_status(self, interaction: nextcord.Interaction):
         """placeholder method"""# Access the specific data you want to include in the embed
 
-        response = make_authenticated_request("GET", f"https://data.cftools.cloud/v1/gameserver/4a1c3f05ef5f6f7004286b8c8f73ef1061e54e1a", token=TOKEN, json=lb_payload)
+        token = authenticate()
+        response = make_authenticated_request("GET", f"https://data.cftools.cloud/v1/gameserver/4a1c3f05ef5f6f7004286b8c8f73ef1061e54e1a", token=token)
         data = response.json()
         server_name = data['4a1c3f05ef5f6f7004286b8c8f73ef1061e54e1a']['name']
         server_status = data['4a1c3f05ef5f6f7004286b8c8f73ef1061e54e1a']['status']['online']
