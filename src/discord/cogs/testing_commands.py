9523d03a0613de0039397c630db4cc1a005d0f8f
Rdoolittle <Rdoolittle90@gmail.com>
2023-03-11 18:51:49 -0600

    remove test

--- src/discord/cogs/testing_commands.py
+++ src/discord/cogs/testing_commands.py
@@ -25,51 +25,6 @@ class TestingCog(commands.Cog):
         await interaction.followup.send(embed=await player_trade(self.bot, interaction.user, player_1_map.title(), player_2, player_2_map.title(), trade_amount))
 
 
-    @slash_command(dm_permission=False, name="test_embed", description="placeholder description 1")
-    async def test_embed(self, interaction: nextcord.Interaction):
-        self.bot.get_settings_json()
-        
-        message_id = None
-        if interaction.user.id in self.bot.settings["persistent_messages"].keys():
-            message_id = self.bot.settings["persistent_messages"][interaction.user.id]["message_id"]
-            number = self.bot.settings["persistent_messages"][interaction.user.id]["number"]
-            self.bot.update_settings_file()
-
-        embed = Embed(title="Persistent Embed", description="This embed is persistent.")
-        if message_id is None:
-            # Send new message
-            self.bot.settings["persistent_messages"][interaction.user.id] = {}
-            self.bot.settings["persistent_messages"][interaction.user.id]["number"] = 0
-            embed.add_field(name="Number", value=self.bot.settings["persistent_messages"][interaction.user.id]["number"])
-            message = await interaction.channel.send(embed=embed)
-            self.bot.settings["persistent_messages"][interaction.user.id]["message_id"] = message.id
-            with open('message_id.json', 'w') as f:
-                json.dump({'message_id': message.id}, f)
-            self.bot.update_settings_file()
-            
-        else:
-            # Edit existing message
-            embed.add_field(name="Number", value=number)
-            message = await interaction.channel.fetch_message(message_id)
-            await message.edit(embed=embed)
-
-        # Add buttons
-        button = Button(style=ButtonStyle.primary, label="Increment", custom_id="increment")
-        view = nextcord.ui.View()
-        view.add_item(button)
-        await message.edit(view=view)
-
-    @commands.Cog.listener()
-    async def on_button_click(self, interaction: nextcord.Interaction):
-        if interaction.custom_id == "increment":
-            self.bot.settings["persistent_messages"][interaction.user.id]["number"] += 1
-            number = self.bot.settings["persistent_messages"][interaction.user.id]["number"]
-            embed = Embed(title="Persistent Embed", description="This embed is persistent.")
-            embed.add_field(name="Number", value=f"{number - 1} -> {number}")
-            self.bot.update_settings_file()
-            await interaction.message.edit(embed=embed)
-            await interaction.response.defer()
-
 
 def setup(bot: commands.Bot):
     bot.add_cog(TestingCog(bot))
