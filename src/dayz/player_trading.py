a323b50d0fa521f0608b07f57ea4b6bc2c944b8a
Rdoolittle <Rdoolittle90@gmail.com>
2023-04-09 20:24:57 -0500

    adding admin give cash command

--- src/dayz/player_trading.py
+++ src/dayz/player_trading.py
@@ -1,4 +1,5 @@
 import ftplib
+from nextcord import Color
 
 import nextcord
 from nextcord import User
@@ -77,6 +78,45 @@ async def player_trade(bot: DiscordBot, player_1:User, player_1_map:str, player_
     elif not player_1_success and not player_2_success:
         embed.description = f"Trade Cancelled 🟠 {reason}"
         embed.color = nextcord.Color.orange()
+    else:
+        embed.description = f"Trade Failed 🔴"
+        embed.color = nextcord.Color.red()
+    return embed
+
+
+async def player_give(bot: DiscordBot, player_2:User, player_2_map:str, trade_amount:int):
+    trade_id = generate_random_string(7)
+    proceed_with_trade = True
+    valid_maps = ["Chernarus", "Takistan", "Namalsk"]
+    if player_2_map not in valid_maps:
+        reason = f"Map Not Found: {player_2_map}\n options: {', '.join(valid_maps)}"
+        proceed_with_trade = False
+
+    player_2_steam_64_id = await get_registered_steam_64(bot, player_2.id)
+    if player_2_steam_64_id is None:
+        reason = "The person you are attempting to send rubles to is not registered"
+        proceed_with_trade = False
+        embed = nextcord.Embed(title=reason, color=Color.red())
+        return embed
+
+
+
+    if proceed_with_trade:
+        player_2_path = f"_files/maps/{player_2_map}/atms/{player_2}.json"
+        player_2_atm = await bot.ftp_connections[player_2_map].download_one_map_file_async("atm", player_2_steam_64_id)
+        update_money(player_2_atm, player_2_path, trade_amount)
+        await bot.ftp_connections[player_2_map].upload_file(player_2_path, "atm", f"{player_2_steam_64_id}.json")
+        colorized_print("INFO", f"trade_id: {trade_id} 🟢 Trade Complete ADMIN -> {player_2.mention}: {trade_amount}")
+        player_2_success = True
+
+    embed = nextcord.Embed(title="Trade Information")
+    embed.add_field(name=f"{player_2}", value=player_2_map, inline=True)
+    embed.add_field(name="Amount:", value=f"{trade_amount}₽")
+    embed.set_footer(text=f"TRADE ID: {trade_id}")
+
+    if player_2_success:
+        embed.description = f"Trade Successful 🟢"
+        embed.color = nextcord.Color.green()
     else:
         embed.description = f"Trade Failed 🔴"
         embed.color = nextcord.Color.red()
--- src/discord/cogs/testing_commands.py
+++ src/discord/cogs/testing_commands.py
@@ -7,7 +7,7 @@ from nextcord import (Button, ButtonStyle, Embed, Interaction, User,
                       slash_command)
 from nextcord.ext import commands
 
-from src.dayz.player_trading import player_trade
+from src.dayz.player_trading import player_give, player_trade
 from src.discord.bot import DiscordBot
 from src.helpers.colored_printing import colorized_print
 
@@ -24,6 +24,17 @@ class TestingCog(commands.Cog):
         await interaction.response.defer(ephemeral=False)
         try:
             embed = await player_trade(self.bot, interaction.user, player_1_map.title(), player_2, player_2_map.title(), trade_amount)
+        except:
+            raise commands.CommandError('Something went wrong.')
+        await interaction.followup.send(embed=embed)
+
+    # =====================================================================================================
+    @slash_command(dm_permission=False, name="trade", description="placeholder description 1")
+    async def give(self, interaction: Interaction, player_2:User, player_2_map:str, trade_amount:int):
+        colorized_print("WARNING", f"{interaction.user.name} used {self}.{inspect.currentframe().f_code.co_name} at {datetime.datetime.now()}")
+        await interaction.response.defer(ephemeral=False)
+        try:
+            embed = await player_give(self.bot, player_2, player_2_map.title(), trade_amount)
         except:
             raise commands.CommandError('Something went wrong.')
         await interaction.followup.send(embed=embed)
